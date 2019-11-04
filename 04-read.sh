998c66770e434d5c1ed4189d7ecc4fd77f1d2e00
Rajani Hub <ryhub42@gmail.com>
2019-11-04 18:51:02 -0500

    2

--- 04-read.sh
+++ 04-read.sh
@@ -3,4 +3,4 @@
 read -p 'Enter the username to add: ' user_name
 read -p 'Enter the group to add: ' group_name
 
-echo following command will be executed=> useradd -g $groupname $username
+echo "following command will be executed=> useradd -g $groupname $username"
