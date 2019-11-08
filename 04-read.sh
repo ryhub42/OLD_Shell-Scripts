023e9ee71b456222e9b5eb4ac8e02bf6164fbbbc
Rajani Hub <ryhub42@gmail.com>
2019-11-08 14:21:31 -0500

    19

--- 04-read.sh
+++ 04-read.sh
@@ -4,11 +4,11 @@
 user_age = 6
 #echo "following command will be executed=> useradd -g $group_name $user_name"
 
-if [ $user_age -eq 6 ] then
+if [ $user_age -eq 6 ]; then
     echo -e "Hello \e[35m$user_name\e[0m], you must be in \e[31mFirst grade!\e[0m. How do you like it??"
-elif [ $user_age -lt 6 ] then
+elif [ $user_age -lt 6 ]; then
  echo -e "Hello You must be kidding"
-elif [ $user_age -gt 6 ] then
+elif [ $user_age -gt 6 ]; then
  #echo -e "what??? No way...."
 else
  echo -e "\e[32mHave a lovely day $user_name\e[0m"
