1f311b84fbb6a3b27831acad6bcbfe3bc3578073
Rajani Hub <ryhub42@gmail.com>
2019-11-08 13:05:06 -0500

    12

--- 04-read.sh
+++ 04-read.sh
@@ -5,11 +5,11 @@ read -p 'How old are you?: ' user_age
 
 #echo "following command will be executed=> useradd -g $group_name $user_name"
 
-if [-z "$user_age" lt 6]; then
+if [[-z "$user_age" lt 6]]; then
 echo -e "You silly! you are lying!!"
-elif [$user_age eq 6]; then
+elif [[$user_age eq 6]]; then
 echo -e "Hello \e[35m$user_name\e[0m], you must be in \e[31mFirst grade!\e[0m. How do you like it??"
-elif [$user_age gt 6]; then
+elif [[$user_age gt 6]]; then
 echo -e "what??? No way...."
 else
 echo -e "\e[32mHave a lovely day $user_name\e[0m"
