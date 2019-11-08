95afdc7d55f9f4519a703e5b91e9a193ef68b20e
Rajani Hub <ryhub42@gmail.com>
2019-11-07 23:00:48 -0500

    5

--- 06-functions.sh
+++ 06-functions.sh
@@ -10,5 +10,16 @@ SAMPLE1 (){
     echo First Argument = $1
 }
 
+SAMPLE2(){
+    A=100
+    local a=200
+}
+
 SAMPLE
 SAMPLE1 abc
+SAMPLE2
+ECHO "A = $A, a = $a"
+
+SAMPLE3(){
+    echo exist status of SAMPLE3 = $?
+}
