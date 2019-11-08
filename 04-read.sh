d0c8f2b3b4b145caa0703408842fe79dee4d9f00
Rajani Hub <ryhub42@gmail.com>
2019-11-08 14:34:36 -0500

    24

--- 04-read.sh
+++ 04-read.sh
@@ -9,10 +9,9 @@ if [ $user_age -eq 6 ]; then
     echo -e "Hello \e[35m$user_name\e[0m, you must be in \e[31mFirst grade!\e[0m. How do you like it??"
 elif [ $user_age -lt 6 ]; then
  echo -e "Hello You must be kidding"
-elif [ $user_age -gt 6 ]; then
+else [ $user_age -gt 6 ]; then
  echo -e "what??? No way...."
  sleep 2
  echo -e "\e[33mSecond Grader!!"
-else
 fi
 echo -e "\e[32mHave a lovely day $user_name\e[0m"
