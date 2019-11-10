b250e63573f5896f9bbe900a35b4e5d993c5b975
Rajani Hub <ryhub42@gmail.com>
2019-11-10 12:57:24 -0500

    7

--- pc-start.sh
+++ pc-start.sh
@@ -12,9 +12,23 @@ START_F() {
  for service in $START_ORDER ; do 
  SERVICE=$(echo $service | awk -F : '{print $1}')
  PORT_NO=$(echo $service | awk -F : '{print $2}')
-  echo "Starting $service Service"
+  echo -n "Starting $service Service"
   su - $USER_NAME -c "nohup java -jar /home/$USER_NAME/spring-petclinic-$service.jar &>/home/$USER_NAME/$service.log &"
   
+  local i=0
+  while [ $i -lt $START_TIMEOUT ]; then
+   netstat -lntp | grep $PORT_NO &>/dev/null
+    if [ $? -eq 0 ]; then
+      echo " -STARTED"
+      break
+     else
+      i=$(($i+15))
+      if [ $i -gt $START_TIMEOUT ]; then
+        echo " - FAILED"
+        exit 1
+      fi
+      sleep 15
+    fi
  done
 }
 
