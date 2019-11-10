465e39297184c26d7a07594fe77affa19a3c5d9b
Rajani Hub <ryhub42@gmail.com>
2019-11-10 15:03:15 -0500

    3

--- studentapp/install.sh
+++ studentapp/install.sh
@@ -28,7 +28,24 @@ STAT_CHECK(){
  fi
 }
 
+## Main Program
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+ echo -e "You should be root user to proceed!"
+ exit 1
+fi
+
 Head "WEB SERVER SETUP"
 Print "Install Web Server"
 yum install nginx -y &>>$LOG
+STAT_CHECK $?
+
+Print "Clean old index files"
+rm -rf /usr/share/nginx/html/* &>>LOG
+STAT_CHECK $?
+
+cd /usr/share/nginx/html/
+
+Print "Download Index files"
+curl -s https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz 
 STAT_CHECK $?
