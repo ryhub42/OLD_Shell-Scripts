e89dcd72949423cbc659660bac7c555a85233a02
Rajani Hub <ryhub42@gmail.com>
2019-11-10 12:44:08 -0500

    3

--- pc-start.sh
+++ pc-start.sh
@@ -2,15 +2,19 @@
 
 ACTION=$1
 USER_NAME=petclinic
-START_ORDER="config-server discovery-server api-gateway vets-service visits-service customers-service"
+START_ORDER="config-server:8888 discovery-server:8761 api-gateway:8080 vets-service:8080 visits-service:8080 customers-service:8080"
 STOP_ORDER="customers-service visits-service vets-service api-gateway discovery-server config-server"
+
+START_TIMEOUT=300
 
 ##Start Function
 START_F() {
  for service in $START_ORDER ; do 
+ SERVICE=$(echo $service | awk -F : '{print $1}')
+ PORT_NO=$(echo $service | awk -F : '{print $2}')
   echo "Starting $service Service"
   su - $USER_NAME -c "nohup java -jar /home/$USER_NAME/spring-petclinic-$service.jar &>/home/$USER_NAME/$service.log &"
-  sleep 1
+  
  done
 }
 
