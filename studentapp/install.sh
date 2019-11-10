d3eb9816afb960d8f962663c88e537972db1c71d
Rajani Hub <ryhub42@gmail.com>
2019-11-10 17:41:14 -0500

    4

--- studentapp/install.sh
+++ studentapp/install.sh
@@ -52,3 +52,14 @@ cd /usr/share/nginx/html/
 Print "Download Index files"
 curl -s https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz 
 STAT_CHECK $?
+
+Print "Update nginx proxy config"
+LINE_NO=$(cat -n /etc/nginx/nginx.conf | grep 'error_page 404' | grep -v '#' | awk '{print$1}')
+ sed -i -e "/^#STARTPROXYCONFIG/,/^#STOPPROXYCONFIG/ d" /etc/nginx/nginx.conf;
+ sed -i -e "$LINE_NO i #STARTPROXYCONFIG\n\tlocation /student {\n\t\tproxy_pass http://localhost:8080/student;\n\t}\n#STOPPROXYCONFIG" /etc/nginx/nginx.conf
+STAT_CHECK $?
+
+Print "Starting Nginx Service"
+systemctl enable nginx &>>$LOG
+systemctl start nginx &>>$LOG
+STAT_CHECK $?
