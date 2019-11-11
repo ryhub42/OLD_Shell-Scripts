d90e19bbd2a5db8bbe74ec1218bf7f8d5055fef3
Rajani Hub <ryhub42@gmail.com>
2019-11-10 19:16:25 -0500

    8

--- studentapp/install.sh
+++ studentapp/install.sh
@@ -130,6 +130,6 @@ systemctl start mariadb &>>$LOG
 STAT_CHECK $?
 
 Print "Load Schema\t\t"
-curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/studentapp-ui-proj1.sql -O /tmp/studentapp.sql
+curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/studentapp-ui-proj1.sql -o /tmp/studentapp.sql
 mysql </tmp/schema.sql
 STAT_CHECK $?
