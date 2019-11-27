129dab63f8eb0cb7f6f8d5b4023859d4e2302650
Rajani y <ryhub42@gmail.com>
2019-11-27 18:33:38 -0500

    new

--- studentapp/webapp.sh
+++ studentapp/webapp.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-## Purpose : Setup student application with web + app + db components
-## Project : STudentApp Monolithic
-## Author  : Copied
-
-##Description: This script installs and configures all web, app and db components 
-
-## Global Variables
-LOG=/tmp/$FUSERNAME.log
+## Purpose : Setup Student Application with Web + App + DB Componenets
+## Project : StudentApp Monolithic
+## Author : Blah Blah
+## Description: This script installs and configures all web components, app components and db components.
+##              Complete application setup will be taken care by this script.
+### Global Variables
+LOG=/tmp/student.log
 rm -f $LOG
 G="\e[32m"
 R="\e[31m"
@@ -16,32 +14,30 @@ FUSERNAME=student
 TOMCAT_VERSION=8.5.47
 TOMCAT_URL=http://apachemirror.wuchna.com/tomcat/tomcat-8/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 TOMCAT_HOME=/home/$FUSERNAME/apache-tomcat-${TOMCAT_VERSION}
-
-## Functions
+### Functions
 Head() {
- echo -e "\n\t\t\t\e[1;35m $1 \e[0m\n"
+  echo -e "\n\t\t\t\e[1;4;35m $1 \e[0m\n"
 }
 
-Print(){
- echo -e "\n\n#............... $1 .............#" >>$LOG
- echo -e -n "  $1\t\t "
- }
-
-STAT_CHECK(){
- if [ $1 -eq 0 ]; then
-  echo -e " - ${G}SUCCESS${N}"
- else
-  echo -e " - ${R}FAILURE${N}"
-  echo -e "Refer Log :: $LOG for more info"
-  exit 1
- fi
+Print() {
+  echo -e "\n\n#--------- $1 ---------#" >>$LOG
+  echo -e -n "  $1\t\t "
 }
 
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
+    echo -e " - ${G}SUCCESS${N}"
+  else
+    echo -e " - ${R}FAILURE${N}"
+    echo -e "Refer Log :: $LOG for more info"
+    exit 1
+  fi
+}
 ## Main Program
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
- echo -e "You should be root user to proceed!"
- exit 1
+  echo -e "You should be root user to proceed!!"
+  exit 1
 fi
 
 Head "WEB SERVER SETUP"
@@ -49,35 +45,34 @@ Print "Install Web Server\t"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-Print "Clean old index files\t"
+Print "Clean old Index files\t"
 rm -rf /usr/share/nginx/html/* &>>$LOG
 STAT_CHECK $?
-
 cd /usr/share/nginx/html/
 
 Print "Download Index files\t"
-curl -s https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz 
+curl -s https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz
 STAT_CHECK $?
 
 Print "Update nginx proxy config"
-LINE_NO=$(cat -n /etc/nginx/nginx.conf | grep 'error_page 404' | grep -v '#' | awk '{print$1}')
- sed -i -e "/^#STARTPROXYCONFIG/,/^#STOPPROXYCONFIG/ d" /etc/nginx/nginx.conf;
- sed -i -e "$LINE_NO i #STARTPROXYCONFIG\n\tlocation /student {\n\t\tproxy_pass http://localhost:8080/student;\n\t}\n#STOPPROXYCONFIG" /etc/nginx/nginx.conf
+LINE_NO=$(cat -n /etc/nginx/nginx.conf | grep 'error_page 404' | grep -v '#' |awk '{print $1}')
+sed -i -e "/^#STARTPROXYCONFIG/,/^#STOPPROXYCONFIG/ d" /etc/nginx/nginx.conf
+sed -i  -e "$LINE_NO i #STARTPROXYCONFIG\n\tlocation /student {\n\t\tproxy_pass http://localhost:8080/student;\n\t}\n#STOPPROXYCONFIG" /etc/nginx/nginx.conf
 STAT_CHECK $?
 
 Print "Starting Nginx Service"
 systemctl enable nginx &>>$LOG
-systemctl start nginx &>>$LOG
+systemctl restart nginx &>>$LOG
 STAT_CHECK $?
 
 Head "APPLICATION SERVER SETUP"
 Print "Adding Functional User"
 id $FUSERNAME &>>$LOG
 if [ $? -eq 0 ]; then
- STAT_CHECK 0
+  STAT_CHECK 0
 else
- useradd $FUSERNAME &>>$LOG
- STAT_CHECK $?
+  useradd $FUSERNAME &>>$LOG
+  STAT_CHECK $?
 fi
 
 Print "Install Java\t\t"
@@ -104,11 +99,10 @@ cd $TOMCAT_HOME
 sed -i -e '/TestDB/ d' -e '$ i <Resource name="jdbc/TestDB" auth="Container" type="javax.sql.DataSource" maxTotal="100" maxIdle="30" maxWaitMillis="10000" username="DBUSER" password="DBPASS" driverClassName="com.mysql.jdbc.Driver" url="jdbc:mysql://DBHOST:3306/DBNAME"/>' conf/context.xml
 STAT_CHECK $?
 
-sed -i -e "s/DBUSER/$1/" -e "s/DBPASS/$2/" -e "s/DBHOST/$3/" -e "s/DBNAME/$4" conf/context.xml
-
+sed -i -e "s/DBUSER/$1/" -e "s/DBPASS/$2/" -e "s/DBHOST/$3/" -e "s/DBNAME/$4/" conf/context.xml
 chown $FUSERNAME:$FUSERNAME /home/$FUSERNAME -R
 
-Print "Download Tomcat init script"
+Print "Downlaod Tomcat init script"
 curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/tomcat-init -o /etc/init.d/tomcat
 STAT_CHECK $?
 
@@ -118,13 +112,12 @@ systemctl daemon-reload &>>$LOG
 STAT_CHECK $?
 
 Print "Start Tomcat Service\t"
-systemctl enable tomcat &>>$LOG
+stemctl enable tomcat &>>$LOG
 systemctl restart tomcat &>>$LOG
 STAT_CHECK $?
 
-
 Print "Load Schema\t\t"
 yum install mariadb -y &>>$LOG
-curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/studentapp-ui-proj1.sql -o /tmp/schema.sql
+curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/studentapp-ui-proj1.sql  -o /tmp/schema.sql
 mysql -h $3 -u$1 -p$2 </tmp/schema.sql
 STAT_CHECK $?
